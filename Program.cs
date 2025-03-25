1af2bb2d9060d8dec1044cfe9ebbf56a2798ebff
nickdermenjivr <nickdermenji@gmail.com>
2025-03-25 12:24:08 +0200

    add icon

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
-Console.WriteLine("Hello, World!");
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient("7598881267:AAFFEJnJOgV-6evab2ElT2zJd11jc01s4AQ", cancellationToken: cts.Token);
+var me = await bot.GetMe();
+bot.OnMessage += OnMessage;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel(); // stop the bot
+
+// method that handle messages received by the bot:
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text is null) return;	// we only handle Text messages here
+    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+    // let's echo back received text in the chat
+    await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
+}
