a34d02224a8eaf8e67201d5a5c2204800f0a8ac5
nickdermenjivr <nickdermenji@gmail.com>
2025-04-08 13:20:37 +0300

    Update playwright

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using GagauziaChatBot.Core.Services.CommandsService;
 using GagauziaChatBot.Core.Services.DiscountsService;
 using GagauziaChatBot.Core.Services.NewsService;
+using Microsoft.Playwright;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -11,6 +12,7 @@ internal static class Program
 {
     private static async Task Main()
     {
+        await TestPlaywright();
         var botToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
         //var botToken = "8181148069:AAHGwZXVK1rLdQ45g5D8_KCf2BPqZ1Q_IkE"; //Test bot
         if (string.IsNullOrEmpty(botToken))
@@ -29,7 +31,7 @@ internal static class Program
 
         await RegisterBotCommands(botClient, cts.Token);
         botService.StartReceiving();
-        botService.StartDiscountsPostingJob();
+        //botService.StartDiscountsPostingJob();
         //botService.StartNewsPostingJob();
 
         var me = await botClient.GetMe(cancellationToken: cts.Token);
@@ -49,5 +51,37 @@ internal static class Program
             commands: commands,
             cancellationToken: cancellationToken
         );
+    }
+
+    private static async Task TestPlaywright()
+    {
+        // Выводим сообщение, чтобы убедиться, что метод вызывается
+        Console.WriteLine("Запуск Playwright...");
+
+        try
+        {
+            var playwright = await Playwright.CreateAsync();
+            Console.WriteLine("Playwright создан.");
+
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+            Console.WriteLine("Браузер запущен.");
+
+            var page = await browser.NewPageAsync();
+            Console.WriteLine("Создана новая страница.");
+
+            await page.GotoAsync("https://example.com");
+            Console.WriteLine("Перешли на страницу https://example.com");
+
+            // Делаем скриншот страницы для дебага
+            await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+            Console.WriteLine("Скриншот сделан.");
+
+            await browser.CloseAsync();
+            Console.WriteLine("Браузер закрыт.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
     }
 }
